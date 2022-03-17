3d96e491c2dd3d902b6e234da15abedf71b63f60
Mogamat-Muneeb <davimuneeb785@gmail.com>
2022-03-17 06:18:04 +0200

    barbers,profile added but btns on profile not working

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,29 +1,42 @@
 <template>
+ 
   <div class="container">
-    <header class="jumbotron">
-      <h1>PROFILE</h1>
-    </header>
-     <div>
-      <strong class="name">Fullname:</strong>
+  <h1 class="jumbotron">PROFILE</h1>
+    
+   
+    <div class="row p-5">
+      <div class="col">
+         <img src="https://i.postimg.cc/q7zMKRBh/blank-profile-picture-973460-640.png" alt="">
+      <div class="picInfo">
       <h6>{{ currentUser.customername }}</h6>
     </div>
-    <div>
-      <strong class="name">Token:</strong>
-      <h6>{{ currentUser.accessToken.substring(0, 20) }} ...</h6>
-      <h6>{{ currentUser.accessToken.substr(currentUser.accessToken.length - 20) }}</h6>
-    </div>
-    <div>
-      <strong class="name">User ID:</strong>
-      <h6>{{ currentUser._id }}</h6>
-    </div>
-    <div>
-      <strong class="name">Email:</strong>
+     <div  class="picInfo">
       <h6>{{ currentUser.email}}</h6>
     </div>
-     <!-- <div>
-      <strong class="name">Phone Number:</strong>
-      <h6>{{ currentUser.number }}</h6>
-    </div> -->
+    </div>
+    <div class="col">
+      <div class="about-profile">   
+        <ul class="admin-profile">
+
+          <li><span class="pro-title"> EMAIL </span> <span class="pro-detail">{{ currentUser.email}}</span></li>
+          <li><span class="pro-title"> FULLNAME </span> <span class="pro-detail">{{ currentUser.customername }}</span></li>
+          <li><span class="pro-title"> TOKEN </span> <span class="pro-detail">{{ currentUser.accessToken.substring(0, 20) }} ...{{ currentUser.accessToken.substr(currentUser.accessToken.length - 20) }}</span></li>
+          <li><span class="pro-title"> USER ID </span> <span class="pro-detail">{{ currentUser._id }}</span></li>
+          
+        </ul>
+        <div class="buttons">
+          <button class="btn">EDIT</button>
+        <button class="btn">DELETE</button>
+        </div>
+        
+      </div>
+    </div>
+
+      
+    </div>
+  
+ 
+    
   </div>
 </template>
 <script>
@@ -43,16 +56,66 @@ export default {
 </script>
 
 <style scoped>
+.buttons{
+  display: flex;
+  padding-top: 20px;
+}
 .container {
-    /* display: flex; */
-    /* justify-content: flex-start; */
+    /* display:;
+    justify-content:center; */
     text-align: center;
-    margin-top: 130px;
+    padding-top: 100px;
    
 }
+.picInfo{
+  padding-top:5px;
+}
 .name{
-  color: #1e90ff;
-   padding-right: 10px;
+  color: #b68345;
+  padding-right: 10px;
+  font-size:20px;
+}
+.admin-profile{
+    display: grid;
+    justify-content: center;
+    flex-wrap: wrap;
+    /* padding-left: 30px;
+    padding-bottom: 40px; */
+}
+.about-profile{
+  padding-top: 10px;
+}
+.about-profile li{
+    display: flex;
+    /* justify-content:space-between; */
+    width:300px;
+    padding-top: 20px;
+}
+.about-profile .pro-title {
+    font-weight: 700;
+    background: transparent;
+    position: relative;
+    width: 120px;
+    display: inline-block;
+    margin-bottom: 5px;
+    
+}
+
+.pro-title{
+    margin-left: -183px;
+}
+
+.about-profile .pro-title:after {
+    position: absolute;
+    content: ':';
+    color: #b68345;
+    font-size: 14px;
+    left: 100px;
+   
+}
+
+ul li, ol li {
+    list-style: none;
 }
 .jumbotron{
   display: flex;
@@ -61,4 +124,14 @@ export default {
 h6{ 
   padding-right: 10px;
 }
+/* .jumbotron{
+   padding-top:100px;
+} */
+img{
+  border-radius: 50%;
+  width: 250px;
+  height: 250px;
+  border: 2px solid #b68345;
+}
+
 </style>
--- src/views/Story.vue
+++ src/views/Story.vue
@@ -6,30 +6,42 @@
             <div class="col-12">
                 <h5>Established in 2022 The Urban Shave Barbershop is a full-service barbershop that is dedicated to providing exponential customer service to our clients alongside trendy haircuts with specified grooming. Our shop is a client-driven relaxing atmosphere in which we want every person who walks through the door feeling delighted upon leaving after giving them what they were looking for. We take great pride in our craft and know what to deliver to one and every client, due to Vancouver being a very diverse city we have people coming through our doors from the ages of one getting that first cut up to the eldest clients that have come in up to 100. In addition to haircuts, the shop offers all the traditional barbershop services including Straight-razor shaves, Beard trims, Beard Fades, Old School Razor Skin Fades, Classic Cut, Buzz Cut. </h5>
             </div>
-            <div class="col-12 p-5">
-                 <h1>Meet The Team</h1>
+   
+    <h1 class="p-5">Meet The Team</h1>
                  <!-- pics of babers -->
-            
-                     <img src="https://i.postimg.cc/1tXgNbV3/naaim.jpg" alt="">
-                 
-                
-                     <img src="https://i.postimg.cc/kgCvtXJM/owner.jpg" alt="">
-                 
-                
-                     <img src="https://i.postimg.cc/L6tzRKzc/yaaseen.jpg" alt="">
-                 
-                
-                    <img src="https://i.postimg.cc/d3MyP4DQ/nadeem1.jpg" alt="">
-                 
-                 
-                 
-                
-                 
+    <div class="col">
+        <div class="image">
+           <img  src="https://i.postimg.cc/1tXgNbV3/naaim.jpg" alt="">
+           <h6 class="centered">Naaim Fredericks, Barber</h6>              
+        </div>
+    </div>             
+    <div class="col">
+         <div class="image">
+            <img  src="https://i.postimg.cc/kgCvtXJM/owner.jpg" alt="">
+            <h6 class="centered">M.Muneeb Davids, Owner</h6>
+        </div>
+    </div>        
+                     
+    <div class="col">
+         <div class="image">
+            <img  src="https://i.postimg.cc/L6tzRKzc/yaaseen.jpg" alt="">
+            <h6 class="centered">Yaaseen Davids, Barber</h6>
+        </div>
+    </div>       
+               
+    <div class="col">
+        <div class="image">
+            <img  src="https://i.postimg.cc/d3MyP4DQ/nadeem1.jpg" alt="">
+            <h6 class="centered">Nadeem Johnson, Barber</h6>
+        </div>
+    </div>                  
+           
+              
             </div>
         </div>
         
        
-    </div>
+   
 </template>
 
 <script>
@@ -43,9 +55,30 @@ export default {
     padding-top:100px;
     
 }
-
+/* .centered {
+  position: absolute;
+  top: 80%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+} */
+.image {
+  position: relative;
+  /* text-align: center; */
+  color: white;
+    max-width: 800px; 
+  margin: 0 auto; 
+}
+.image .centered {
+  position: absolute;
+  bottom: 0; 
+  background: rgb(0, 0, 0); 
+  background: rgba(0, 0, 0, 0.5); 
+  width: 100%;
+  padding: 20px; 
+  margin-bottom: 0;
+}
 .container{
-    display:flex;
+    display:grid;
     justify-content:center;
 }
 h5{
@@ -56,6 +89,6 @@ img{
     width:300px;
     height:350px;
     object-fit:cover;
-    padding:5px
+    /* padding:5px */
 }
 </style>

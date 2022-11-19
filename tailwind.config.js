f85c0840d8fdccbec1ab6abc693b0622864b3b57
Mogamat-Muneeb <davimuneeb785@gmail.com>
2022-11-19 08:46:14 +0200

    wip:added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: ['./index.html'],
+     purge: [],
+     darkMode: false, // or 'media' or 'class'
+     theme: {
+       fontFamily:{
+         helvetica:[ 'Helvetica-Bold','sans-serif'],
+         helveticaNorm:[ 'Helvetica','sans-serif'],
+       }
+     },
+     variants: {
+       extend: {},
+     },
+     plugins: [],
+   }
+   

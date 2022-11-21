d827ffc3233a9377bd4cf5e90d65b4479f7dad8f
mmuneebwebwiser <davimuneeb785@gmail.com>
2022-11-21 14:25:10 +0200

    wip

--- src/views/Story.vue
+++ src/views/Story.vue
@@ -1,48 +1,64 @@
 <template>
-
-    <h1 class="head" style="font-weight: bold;color:#b18044">OUR STORY</h1>
-    <div class="border"></div>
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h5 style="font-size:22px;font-weight: 400;text-align: center;white-space: pre-wrap;">Established in 2022 The Urban Shave Barbershop is a full-service barbershop that is dedicated to providing exponential customer service to our clients alongside trendy haircuts with specified grooming. Our shop is a client-driven relaxing atmosphere in which we want every person who walks through the door feeling delighted upon leaving after giving them what they were looking for. We take great pride in our craft and know what to deliver to one and every client, due to Vancouver being a very diverse city we have people coming through our doors from the ages of one getting that first cut up to the eldest clients that have come in up to 100. In addition to haircuts, the shop offers all the traditional barbershop services including Straight-razor shaves, Beard trims, Beard Fades, Old School Razor Skin Fades, Classic Cut. </h5>
-            </div>
-   
-    <h1 class="p-5">Meet The Team</h1>
-                 <!-- pics of babers -->
-    <div class="col">
-        <div class="image">
-           <img  src="https://i.postimg.cc/1tXgNbV3/naaim.jpg" alt="">
-           <h6 class="centered">Naaim Fredericks, Barber</h6>              
-        </div>
-    </div>             
-    <div class="col">
-         <div class="image">
-            <img  src="https://i.postimg.cc/kgCvtXJM/owner.jpg" alt="">
-            <h6 class="centered">M.Muneeb Davids, Owner/Barber</h6>
-        </div>
-    </div>        
-                     
-    <div class="col">
-         <div class="image">
-            <img  src="https://i.postimg.cc/L6tzRKzc/yaaseen.jpg" alt="">
-            <h6 class="centered">Yaaseen Davids, Barber</h6>
-        </div>
-    </div>       
-               
-    <div class="col">
-        <div class="image">
-            <img  src="https://i.postimg.cc/d3MyP4DQ/nadeem1.jpg" alt="">
-            <h6 class="centered">Nadeem Johnson, Barber</h6>
-        </div>
-    </div>                  
-           
-              
-            </div>
-        </div>
-        
-       
-   
+<div class="flex flex-col items-center justify-center">
+  <div class="">
+  <img src="https://i.postimg.cc/gzZtrrZy/about-banner.png" alt="" class="object-cover  banner">
+  </div>
+  <div class="absolute flex items-center justify bottom-96">
+    <div class="flex flex-col items-center justify-center">
+      <h1 class="font-bold text-white uppercase text-7xl">ABOUT THE URBAN SHAVE</h1>
+       <!-- <h3 class="text-xl font-semibold text-white">Lets Your Hair Do The Talking...</h3> -->
+    </div>
+  </div>
+  <div class="flex flex-col justify-center mx-auto maxWidth mt-10">
+  <h1 class="font-semibold text-3xl">About Us</h1>
+    <p class="text-left">Established in 2022 The Urban Shave Barbershop is a full-service barbershop that is dedicated to providing exponential customer service to our 
+        clients alongside trendy haircuts with specified grooming. Our shop is a client-driven relaxing atmosphere in which we want every person who walks through the 
+        door feeling delighted upon leaving after giving them what they were looking for.
+    </p>
+    <p class="text-left"> We take great pride in our craft and know what to deliver to one and every client, 
+        due to Vancouver being a very diverse city we have people coming through our doors from the ages of one getting that first cut up to the eldest clients that have come in up to 100. 
+        In addition to haircuts, the shop offers all the traditional barbershop services including Straight-razor shaves, Beard trims, Beard Fades, Old School Razor Skin Fades, Classic Cut.</p>
+  
+    <h1 class="font-semibold text-3xl">Meet the team</h1>
+  <div class="grid grid-cols-3 gap-10 mt-10">
+    <div class="flex justify-start">
+        <div class="">
+         <img src="https://i.postimg.cc/bJmFrdsY/Ellipse-1.png" alt="baby with headphones" class="w-36 h-36 rounded-full object-cover">
+         <h1 class="font-semibold text-2xl">Mog Muneeb Davids</h1>
+         <p>079 876 1234</p>
+      </div>
+    </div>
+    <div class="">
+        <div class="">
+         <img src="https://i.postimg.cc/FFnBQfMD/Ellipse-2.png" alt="baby with headphones" class="w-36 h-36 rounded-full object-cover">
+         <h1 class="font-semibold text-2xl">Suhail Davids </h1>
+         <p>079 876 1234</p>
+      </div>
+    </div>
+    <div class=" flex justify-end">
+        <div class="">
+         <img src="https://i.postimg.cc/rFBZKBF4/Ellipse-2-1.png" alt="baby with headphones" class="w-36 h-36 rounded-full object-cover">
+        <h1 class="font-semibold text-2xl">Yaseen Davids</h1>
+         <p>079 876 1234</p>
+      </div>
+    </div>
+    <div class="flex justify-start">
+        <div class="">
+         <img src="https://i.postimg.cc/2yJcdJQP/Ellipse-3.png" alt="baby with headphones" class="w-36 h-36 rounded-full object-cover">
+        <h1 class="font-semibold text-2xl">Naaim Fredericks</h1>
+         <p>079 876 1234</p>
+      </div>
+    </div>
+    <div class="">
+        <div class="">
+         <img src="https://i.postimg.cc/FsdnFrmm/Ellipse-3-1.png" alt="baby with headphones" class="w-36 h-36 rounded-full object-cover">
+        <h1 class="font-semibold text-2xl">Leah Johnson</h1>
+         <p>079 876 1234</p>
+      </div>
+    </div>
+  </div>
+  </div>
+</div>
 </template>
 
 <script>
@@ -52,63 +68,5 @@ export default {
 </script>
 
 <style scoped>
-.head{
-    padding-top:100px;
-    
-}
-/* .centered {
-  position: absolute;
-  top: 80%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-} */
-.image {
-  position: relative;
-  /* text-align: center; */
-  color: white;
-    max-width: 800px; 
-  margin: 0 auto; 
-}
-.image:hover{
-       transform: scale(1.1);
-  transition:.5s;
-}
-.image .centered {
-  position: absolute;
-  bottom: 0; 
-  background: rgb(0, 0, 0); 
-  background: rgba(0, 0, 0, 0.5); 
-  width: 100%;
-  padding: 20px; 
-  margin-bottom: 0;
-}
-.container{
-    display:grid;
-    justify-content:center;
-}
-h5{
-    padding-top: 50px;
-}
 
-img{
-    width:300px;
-    height:350px;
-    object-fit:cover;
-    /* padding:5px */
-}
-
-@media only screen and (max-width: 500px) {
-   img{
-    width:250px;
-    height:350px;
-    object-fit:cover;
-    padding:10px
-}
-}
-.border{
-    border-top: 1px solid;
-    width: 100px;
-    display:inline-flex;
-    justify-content: center;
-}
 </style>
